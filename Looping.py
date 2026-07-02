7cccc5811453cf8bdad14ed1113de2eb45d6c7fb
sthondamal <sthondamal@gmail.com>
2026-07-02 08:34:53 +0530

    Added Looping

--- Looping.py
+++ Looping.py
@@ -1,4 +1,23 @@
-for i in range(7,71,7):
-     print(i)
+#for i in range(7,71,7):
+#     print(i)
 #for i in range(1,11):
     #print(i*7)
+
+#password
+password = "password@123"
+user = " "
+
+while user !=password:
+    user = input("Enter password:")
+
+print("Access Granted")
+
+#Write a program for ATM pin verification
+
+AtmPin = 1234
+pin = " "
+
+while pin != AtmPin:
+    pin = int(input("Enter Pin"))
+
+print("Pin in correct")
